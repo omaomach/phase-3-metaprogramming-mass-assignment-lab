02d87ce43807454290b869e276b2ebc658e11838
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-10-27 02:54:49 +0300

    completed phase 3 metaprogramming mass assignment lab

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,27 @@
 class Person
-  # your code here
+
+  def initialize attributes
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
+
 end
+
+joel = {
+  name: "Asiago",
+  birthday: "27th February",
+  handed: "left",
+  height: 6.0
+}
+
+joash = {
+  name: "Omao",
+  weight: "70kg",
+  handed: "right",
+  height: "6 ft 3"
+}
+
+user1 = Person.new(joash)
+puts user1.handed
